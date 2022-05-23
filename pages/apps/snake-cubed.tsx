4947c7aa80a22d4990f1e80c864580ee1d644014
William Wu <dev.wwu@gmail.com>
2022-05-23 12:47:04 -0700

    Add placeholder site pages

---
+++ pages/apps/snake-cubed.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const About: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Placeholder Snake Cubed page</title>
+      </Head>
+      <main>
+        <h1>Snake Cubed Snake Cubed</h1>
+        <p>Hello world this is the Snake Cubed page</p>
+      </main>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/resources/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const About: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Placeholder Resources page</title>
+      </Head>
+      <main>
+        <h1>Resources resources resources</h1>
+        <p>Hello world this is the Resources page</p>
+      </main>
+    </div>
+  );
+};
+
+export default About;
