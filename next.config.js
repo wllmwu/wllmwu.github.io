6d04a43585ddaaa996fba4b5a6933abcc4a5a859
William Wu <dev.wwu@gmail.com>
2022-06-10 09:38:25 -0700

    Install MDX packages, replace `index.tsx` with `index.mdx`

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
+import nextMDXConfig from "@next/mdx";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: "custom",
   },
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 };
 
-module.exports = nextConfig;
+const withMDX = nextMDXConfig({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+
+module.exports = withMDX(nextConfig);
