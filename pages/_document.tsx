148601a955628dd4f74dcaa4dd595e954998ab0a
William Wu <dev.wwu@gmail.com>
2022-06-10 20:13:56 -0700

    Add styling for headings

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ function MyDocument() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@500&display=optional"
+          href="https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@500;600&display=optional"
           rel="stylesheet"
         />
         <link
