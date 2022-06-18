988f40a904571f77677d7b4dbb073fdfc020fd56
William Wu <dev.wwu@gmail.com>
2022-06-18 10:49:39 -0700

    Add type for page metadata, add page head title tags

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,16 +1,28 @@
 import React from "react";
+import Head from "next/head";
 import Banner from "./Banner";
 import styles from "../styles/Page.module.css";
 
+interface PageMetadata {
+  title: string;
+  seoTitle?: string;
+  showBanner?: boolean;
+}
+
 interface PageProps {
-  metadata: Record<string, string>;
+  metadata: PageMetadata;
   children: React.ReactNode;
 }
+
+const formatPageTitle = (pageTitle: string) => `${pageTitle} | William Wu`;
 
 function Page({ metadata, children }: PageProps) {
   return (
     <div className={styles.page}>
-      <Banner title={metadata.title} />
+      <Head>
+        <title>{formatPageTitle(metadata.seoTitle ?? metadata.title)}</title>
+      </Head>
+      {(metadata.showBanner ?? true) && <Banner title={metadata.title} />}
       <div className={styles.content}>{children}</div>
     </div>
   );
