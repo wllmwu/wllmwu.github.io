31fbe10cf1a8be0df699867ad87604c583168006
William Wu <dev.wwu@gmail.com>
2022-06-18 10:34:30 -0700

    Add `Banner` component

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Banner from "./Banner";
 import styles from "../styles/Page.module.css";
 
 interface PageProps {
@@ -7,7 +8,12 @@ interface PageProps {
 }
 
 function Page({ metadata, children }: PageProps) {
-  return <div className={styles.content}>{children}</div>;
+  return (
+    <div className={styles.page}>
+      <Banner title={metadata.title} />
+      <div className={styles.content}>{children}</div>
+    </div>
+  );
 }
 
 export default Page;
