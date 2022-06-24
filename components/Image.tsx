ab0dbb7df43138c6f885614cf86f61c1f42de5a5
William Wu <dev.wwu@gmail.com>
2022-06-23 17:21:09 -0700

    Try bare `<img>` tag instead of using `next/image`

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
+
 import React from "react";
-import NextImage from "next/image";
+// import NextImage from "next/image";
+import styles from "../styles/Image.module.css";
 
 interface ImageProps {
   source: string;
@@ -8,17 +11,27 @@ interface ImageProps {
   alt?: string;
 }
 
-const loader = ({ src }: { src: string }) => src;
+// const loader = ({ src }: { src: string }) => src;
 
 function Image({ source, width, height, alt }: ImageProps) {
+  // return (
+  //   <NextImage
+  //     loader={loader}
+  //     src={source}
+  //     width={width}
+  //     height={height}
+  //     layout="intrinsic"
+  //     alt={alt}
+  //   />
+  // );
   return (
-    <NextImage
-      loader={loader}
+    <img
       src={source}
+      alt={alt}
+      decoding="async"
       width={width}
       height={height}
-      layout="intrinsic"
-      alt={alt}
+      className={styles.image}
     />
   );
 }
