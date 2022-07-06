2e07d813a356410fe4bfee9ce9f0415729409eca
William Wu <74469686+wllmwu@users.noreply.github.com>
2022-07-05 17:02:49 -0700

    Fix client-side redirects (#57)

--- components/index.tsx
+++ components/index.tsx
@@ -4,4 +4,5 @@ export { default as Image, AppIconImage, AppScreenshotImage } from "./Image";
 export { default as ImageCarousel } from "./ImageCarousel";
 export { default as Layout } from "./Layout";
 export { default as Page } from "./Page";
+export { default as PageRedirect } from "./PageRedirect";
 export { default as PinkSquare } from "./PinkSquare";
