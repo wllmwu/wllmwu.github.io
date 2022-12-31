28411441fb4cc2e667b2d8f3725c8e4b6535a112
William Wu <dev.wwu@gmail.com>
2022-12-30 16:24:14 -0800

    Add new favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,6 +22,13 @@ function MyDocument() {
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wdth@87.5&display=swap"
           rel="stylesheet"
         />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00b0f0" />
+        <meta name="msapplication-TileColor" content="#2d89ef" />
+        <meta name="theme-color" content="#ffffff" />
         <meta httpEquiv="X-Clacks-Overhead" content="GNU Terry Pratchett" />
       </Head>
       <body>
