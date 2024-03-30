7f8e4470c737a9438b4ea1224df8760b86297ad5
William Wu <dev.wwu@gmail.com>
2024-03-29 21:05:20 -0700

    Add base config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 // https://mdxjs.com/docs/getting-started/#vite
 export default defineConfig({
+  base: "./",
   plugins: [{ enforce: "pre", ...mdx() }, react()],
 });
