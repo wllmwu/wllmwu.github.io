19458fde0bc6deb272bf4a2054b090dfd4c65e02
William Wu <dev.wwu@gmail.com>
2024-06-24 11:05:17 -0700

    Update `Breadcrumbs` CSS

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -3,6 +3,11 @@ import { UIMatch, useMatches } from "react-router-dom";
 import styles from "../styles/Breadcrumb.module.css";
 import { RouteHandle } from "../types";
 
+/**
+ * Based on the example at https://reactrouter.com/en/main/hooks/use-matches.
+ * Assumes there are no gaps in the breadcrumbs at any point from the root to
+ * the current route.
+ */
 function Breadcrumbs() {
   const matches = useMatches() as UIMatch<unknown, RouteHandle>[];
   const crumbs = matches
