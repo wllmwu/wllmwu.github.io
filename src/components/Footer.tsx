63c3052259b1c478b89b6d44b8f4c15aaff1cc5f
William Wu <dev.wwu@gmail.com>
2024-06-24 14:02:09 -0700

    Update `Footer` and CSS variables

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,13 +3,11 @@ import styles from "./Footer.module.css";
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.footerContent}>
-        <p>{"\u00a9"} 2024 William Wu</p>
-        <p>
-          Site designed and implemented by me. Built with Vite and React, hosted
-          with GitHub Pages. <a href="/privacy">Privacy policies</a>
-        </p>
-      </div>
+      <p>{"\u00a9"} 2024 William Wu</p>
+      <p>
+        Site designed and implemented by me. Built with Vite and React, hosted
+        with GitHub Pages. <a href="/privacy">Privacy policies</a>
+      </p>
     </footer>
   );
 }
