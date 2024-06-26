7e0509cf6f28e8d9baf43a4915ff96d448fbd506
William Wu <dev.wwu@gmail.com>
2024-06-26 09:05:37 -0700

    Use `ReactDOM.createRoot` instead of `.hydrateRoot` in non-SSR environment

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.hydrateRoot(
-  document.getElementById("root")!,
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (import.meta.env.SSR) {
+  ReactDOM.hydrateRoot(
+    document.getElementById("root")!,
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
